f51e667575c08e5f49fa25178356d64c190152f0
clara bayley <yoctoyotta1024@yahoo.com>
2024-02-28 16:35:31 +0100

    unit tests for wrapper and thermo

--- tests/test_microphysics_scheme.py
+++ tests/test_microphysics_scheme.py
@@ -20,14 +20,72 @@ mock unit tests for Python microphysics module
 
 
 from libs.src_py.microphysics_scheme import MicrophysicsScheme
+from libs.src_py.microphysics_scheme_wrapper import MicrophysicsSchemeWrapper
+from libs.src_py.thermodynamics import Thermodynamics
 
-def test_microphys():
+def test_initialize():
 
 	microphys = MicrophysicsScheme()
 
-	assert microphys.initialize() == 0
+	assert microphys.initialize() == None
 
-	assert microphys.run(0,1) == 1
-	assert microphys.run(0,2) == 2
+def test_finalize():
 
-	assert microphys.finalize() == 0
+	microphys = MicrophysicsScheme()
+
+	assert microphys.finalize() == None
+
+def test_initialize_wrapper():
+
+	nvec = 1
+	ke = 1
+	ivstart = 0
+	dz = 10
+	qnc = 500
+	microphys_wrapped = MicrophysicsSchemeWrapper(nvec, ke, ivstart, dz, qnc)
+
+	assert microphys_wrapped.initialize() == 0
+
+def test_finalize_wrapper():
+
+	nvec = 1
+	ke = 1
+	ivstart = 0
+	dz = 10
+	qnc = 500
+	microphys_wrapped = MicrophysicsSchemeWrapper(nvec, ke, ivstart, dz, qnc)
+
+	assert microphys_wrapped.finalize() == 0
+
+def test_microphys_with_wrapper():
+
+	microphys = MicrophysicsScheme()
+
+	nvec = 1
+	ke = 1
+	ivstart = 0
+	dz = 10
+	qnc = 500
+	microphys_wrapped = MicrophysicsSchemeWrapper(nvec, ke, ivstart, dz, qnc)
+
+	timestep = 1.0
+	temp = 288.15
+	rho = 1.225
+	press = 101325
+	qvap = 0.015
+	qcond = 0.0001
+	qice = 0.0002
+	qrain = 0.0003
+	qsnow = 0.0004
+	qgrau = 0.0005
+
+	thermo = Thermodynamics(temp, rho, press, qvap, qcond, qice, qrain, qsnow, qgrau)
+
+	t, qv, qc, qi, qr, qs, qg, prr_gsp, pflx = microphys.run(nvec, ke, ivstart, timestep, dz, temp,
+																													rho, press, qvap, qcond, qice,
+																													qrain, qsnow, qgrau, qnc)
+
+	thermo = microphys_wrapped.run(timestep, thermo)
+
+	assert thermo.temp == t
+	assert thermo.massmix_ratios == [qv, qc, qi, qr, qs, qg]
