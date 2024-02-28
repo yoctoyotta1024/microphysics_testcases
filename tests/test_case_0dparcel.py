757e420865f9aa919281c0eab8e16171a589dc4b
clara bayley <yoctoyotta1024@yahoo.com>
2024-02-28 14:15:50 +0100

    draft of test case

--- tests/test_case_0dparcel.py
+++ tests/test_case_0dparcel.py
@@ -15,16 +15,25 @@ License: BSD 3-Clause "New" or "Revised" License
 https://opensource.org/licenses/BSD-3-Clause
 -----
 File Description:
-test case for adiabatic expansion of a parcel of air (0-D model)
+mock test case for adiabatic expansion of a parcel of air (0-D model)
 '''
 
 
 from libs.src_py.microphysics_scheme import MicrophysicsScheme
 
-def run_0dparcel():
+def run_0dparcel_adiabatic_expansion(time_init, time_end, time_microphys, temp0, press0, microphys):
 
   assert 1+1 == 2
 
 def test_case_0dparcel():
 
-  run_0dparcel()
+  time_init = 0.0 # [s]
+  time_end = 10.0 # [s]
+  time_microphys = 1.0 # [s]
+
+  temp0 = 298.15 # [K]
+  press0 = 101325 # [Pa]
+
+  microphys = MicrophysicsScheme()
+
+  run_0dparcel_adiabatic_expansion(time_init, time_end, time_microphys, temp0, press0, microphys)
