aa6e502865ce75300d0d3541c383eb7f0ba598cb
clara bayley <yoctoyotta1024@yahoo.com>
2024-02-29 15:47:56 +0100

    better equations for adiabatic process

--- tests/test_case_0dparcel/adiabatic_motion.py
+++ tests/test_case_0dparcel/adiabatic_motion.py
@@ -24,46 +24,42 @@ import numpy as np
 class AdiabaticMotion():
   """A class for driving the adiabatic expansion/contraction of a volume of air"""
 
-  def __init__(self, rho_amp, tau, temp_ref, press_ref):
-    """Init the AdiabaticMoetion object """
+  def __init__(self, amp, tau):
+    """Init the AdiabaticMotion object """
 
     rgas_univ = 8.314462618   # universal molar gas constant [J/Kg/K]
     mr_dry = 0.028966216      # molecular mass of dry air [Kg/mol]
     mr_water = 0.01801528     # molecular mass of water [Kg/mol]
 
+    self.cp_dry = 1004.64     # specific heat capacity of water vapour [J/Kg/K] (IAPWS97 at 273.15K)
     self.rgas_dry = rgas_univ / mr_dry   # specific gas constant for dry air [J/Kg/K] (approx. 287 J/Kg/K)
-    self.gamma_dry = 1.400               # ratio of dry air specific heat capacities (cp_dry / cv_dry)
     self.epsilon = mr_water / mr_dry     # ratio of gas constants, dry air / water vapour (approx. 0.622)
 
-    self.rho_amp = rho_amp
-    self.freq = 2.0 * np.pi / tau
+    self.amp = amp # amplitude of pressure sinusoid [Pa]
+    self.omega = 2.0 * np.pi / tau # angular frequency of pressure sinusio (tau is time period) [radians s^-1]
 
-    self.temp_ref = temp_ref
-    self.press_ref = press_ref
+  def dpress_dtime(self, time):
 
-  def kappa_const(self, qvap):
+    dpress_dt = - self.omega * self.amp * np.cos(self.omega * time)
 
-    kappa = self.rgas_dry * self.temp_ref / self.press_ref * (1 + qvap / self.epsilon)
+    return dpress_dt
 
-    return kappa
+  def dtemp_dtime(self, rho, dpress_dt):
 
-  def drho_dt(self, time):
+    dtemp_dt = dpress_dt / rho / self.cp_dry
 
-    drho_dt = -self.freq * self.rho_amp * np.cos(self.freq * time)
+    return dtemp_dt
+
+  def drho_dtime(self, temp, rho, qvap, dpress_dt, dtemp_dt):
+
+    rgas_eff = self.rgas_dry * (1 + qvap / self.epsilon)
+
+    drho_dt_temp = - dtemp_dt * rho / temp
+    drho_dt_press = dpress_dt / rgas_eff / temp
+
+    drho_dt = drho_dt_press + drho_dt_temp
 
     return drho_dt
-
-  def dtemp_drho(self, temp, rho):
-
-    dtemp_drho = (self.gamma_dry - 1) * temp / rho
-
-    return dtemp_drho
-
-  def dpress_drho(self, press, rho):
-
-    dpress_drho = self.gamma_dry * press / rho
-
-    return dpress_drho
 
   def run(self, time, timestep, thermo):
     """Run the adiabatic motion computations.
@@ -72,12 +68,14 @@ class AdiabaticMotion():
 
     """
 
-    delta_rho = self.drho_dt(time) * timestep
-    delta_temp = self.dtemp_drho(thermo.temp, thermo.rho) * delta_rho
-    delta_press = self.dpress_drho(thermo.press, thermo.rho) * delta_rho
+    qvap = thermo.massmix_ratios[0]
 
+    delta_press = self.dpress_dtime(time) * timestep
+    delta_temp = self.dtemp_dtime(thermo.rho, delta_press)
+    delta_rho = self.drho_dtime(thermo.temp, thermo.rho, qvap, delta_press, delta_temp)
+
+    thermo.press += delta_press
     thermo.temp += delta_temp
     thermo.rho += delta_rho
-    thermo.press += delta_press
 
     return thermo
