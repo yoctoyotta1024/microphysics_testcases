4c02f6523a283ef77e239c3d109e53083e780dfc
clara.bayley <yoctoyotta1024@yahoo.com>
2024-09-01 19:43:35 +0200

    refactor: rename files

--- tests/test_mock_py_microphysics_scheme.py
+++ tests/test_mock_py_microphysics_scheme.py
@@ -2,13 +2,13 @@
 Copyright (c) 2024 MPI-M, Clara Bayley
 
 ----- Microphysics Test Cases -----
-File: test_microphysics_scheme.py
+File: test_mock_py_microphysics_scheme.py
 Project: tests
 Created Date: Tuesday 27th February 2024
 Author: Clara Bayley (CB)
 Additional Contributors:
 -----
-Last Modified: Monday 17th June 2024
+Last Modified: Sunday 1st September 2024
 Modified By: CB
 -----
 License: BSD 3-Clause "New" or "Revised" License
