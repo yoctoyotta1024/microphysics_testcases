52d499ddd4859349bb96a79e19f13d2245af445a
clara.bayley <yoctoyotta1024@yahoo.com>
2024-09-01 20:59:36 +0200

    feat: new python file and requirements for KiD test case

---
+++ tests/test_case_1dkid/run_1dkid_test_case.py.py
@@ -0,0 +1,23 @@
+"""
+Copyright (c) 2024 MPI-M, Clara Bayley
+
+----- Microphysics Test Cases -----
+File: run_1dkid_test_case.py.py
+Project: test_case_1dkid
+Created Date: Sunday 1st September 2024
+Author: Clara Bayley (CB)
+Additional Contributors:
+-----
+Last Modified: Sunday 1st September 2024
+Modified By: CB
+-----
+License: BSD 3-Clause "New" or "Revised" License
+https://opensource.org/licenses/BSD-3-Clause
+-----
+File Description:
+"""
+
+# import numpy as np
+# from PyMPDATA import Options
+# from PyMPDATA.impl.enumerations import ARG_DATA_INNER, ARG_DATA, ONE_FOR_STAGGERED_GRID
+# from PyMPDATA_examples import Shipway_and_Hill_2012 as kid
