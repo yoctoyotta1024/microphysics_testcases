56b08284696688c57d5978a618caaf20c59be445
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-06-26 17:14:00 +0200

    refactor: make cleo tests individual

--- tests/test_cleo_sdm_microphysics_scheme.py
+++ tests/test_cleo_sdm_microphysics_scheme.py
@@ -15,10 +15,18 @@ License: BSD 3-Clause "New" or "Revised" License
 https://opensource.org/licenses/BSD-3-Clause
 -----
 File Description:
-unit tests for cleo_sdm microphysics module
+unit tests for cleo_sdm microphysics module.
+NOTE: tests assume CLEO's initial condition binary files already exist
+    (i.e. 'dimlessGBxboundaries.dat' and 'dimlessSDsinit.dat' files, whose
+    locations are given in CLEO's config file ('config_filename')
 """
 
 import pytest
+from mpi4py import MPI
+import os
+import sys
+import numpy as np
+from ruamel.yaml import YAML
 
 
 @pytest.fixture(scope="module")
@@ -31,17 +39,96 @@ def config_filename(pytestconfig):
     return pytestconfig.getoption("cleo_test_generic_config_filename")
 
 
-def test_generic_cleo(path2pycleo, config_filename):
-    """NOTE: tests assume CLEO's initial condition binary files already exist
-    (i.e. 'dimlessGBxboundaries.dat' and 'dimlessSDsinit.dat' files, whose
-    locations are given in CLEO's config file ('config_filename')
-    """
-    # TODO(CB): fix multiple kokkos initialise error -> call each test seperately
-    import os
-    import sys
-    import numpy as np
-    from mpi4py import MPI
+def test_mpi_is_initialised():
+    print(f"MPI version: {MPI.Get_version()}")
 
+
+def test_initialize(path2pycleo, config_filename):
+    os.environ["PYCLEO_DIR"] = str(path2pycleo)
+
+    from libs.cleo_sdm.cleo_sdm import CleoSDM as MicrophysicsScheme
+
+    sys.path.append(os.environ["PYCLEO_DIR"])
+    import pycleo
+
+    yaml = YAML()
+    with open(config_filename, "r") as file:
+        python_config = yaml.load(file)
+
+    t_start = 0
+    timestep = python_config["timesteps"]["COUPLTSTEP"]  # [s]
+    press = np.array([], dtype=np.float64)
+    temp = np.array([], dtype=np.float64)
+    qvap = np.array([], dtype=np.float64)
+    qcond = np.array([], dtype=np.float64)
+    config = pycleo.Config(str(config_filename))
+    pycleo.pycleo_initialize(config)
+    microphys = MicrophysicsScheme(config, t_start, timestep, press, temp, qvap, qcond)
+
+    assert microphys.name == "CLEO SDM microphysics"
+
+
+def test_initialize_wrapper(path2pycleo, config_filename):
+    os.environ["PYCLEO_DIR"] = str(path2pycleo)
+
+    from libs.cleo_sdm.microphysics_scheme_wrapper import MicrophysicsSchemeWrapper
+
+    sys.path.append(os.environ["PYCLEO_DIR"])
+
+    yaml = YAML()
+    with open(config_filename, "r") as file:
+        python_config = yaml.load(file)
+
+    t_start = 0
+    timestep = python_config["timesteps"]["COUPLTSTEP"]  # [s]
+    press = np.array([], dtype=np.float64)
+    temp = np.array([], dtype=np.float64)
+    qvap = np.array([], dtype=np.float64)
+    qcond = np.array([], dtype=np.float64)
+    microphys_wrapped = MicrophysicsSchemeWrapper(
+        config_filename,
+        t_start,
+        timestep,
+        press,
+        temp,
+        qvap,
+        qcond,
+    )
+
+    assert microphys_wrapped.initialize() == 0
+
+
+def test_finalize_wrapper(path2pycleo, config_filename):
+    os.environ["PYCLEO_DIR"] = str(path2pycleo)
+
+    from libs.cleo_sdm.microphysics_scheme_wrapper import MicrophysicsSchemeWrapper
+
+    sys.path.append(os.environ["PYCLEO_DIR"])
+
+    yaml = YAML()
+    with open(config_filename, "r") as file:
+        python_config = yaml.load(file)
+
+    t_start = 0
+    timestep = python_config["timesteps"]["COUPLTSTEP"]  # [s]
+    press = np.array([], dtype=np.float64)
+    temp = np.array([], dtype=np.float64)
+    qvap = np.array([], dtype=np.float64)
+    qcond = np.array([], dtype=np.float64)
+    microphys_wrapped = MicrophysicsSchemeWrapper(
+        config_filename,
+        t_start,
+        timestep,
+        press,
+        temp,
+        qvap,
+        qcond,
+    )
+
+    assert microphys_wrapped.finalize() == 0
+
+
+def test_microphys_with_wrapper(path2pycleo, config_filename):
     os.environ["PYCLEO_DIR"] = str(path2pycleo)
 
     from libs.cleo_sdm.cleo_sdm import CleoSDM as MicrophysicsScheme
@@ -51,143 +138,52 @@ def test_generic_cleo(path2pycleo, config_filename):
     sys.path.append(os.environ["PYCLEO_DIR"])
     import pycleo
 
-    def test_initialize(config_filename):
-        from ruamel.yaml import YAML
+    yaml = YAML()
+    with open(config_filename, "r") as file:
+        python_config = yaml.load(file)
 
-        yaml = YAML()
-        with open(config_filename, "r") as file:
-            python_config = yaml.load(file)
+    t_start = 0
+    timestep = python_config["timesteps"]["COUPLTSTEP"]  # [s]
+    temp1 = np.array([288.15], dtype=np.float64)
+    temp2 = np.array([288.15], dtype=np.float64)
+    rho = np.array([1.225], dtype=np.float64)
+    press1 = np.array([101325], dtype=np.float64)
+    press2 = np.array([101325], dtype=np.float64)
+    qvap1 = np.array([0.015], dtype=np.float64)
+    qvap2 = np.array([0.015], dtype=np.float64)
+    qcond1 = np.array([0.0001], dtype=np.float64)
+    qcond2 = np.array([0.0001], dtype=np.float64)
+    qice = np.array([0.0002], dtype=np.float64)
+    qrain = np.array([0.0003], dtype=np.float64)
+    qsnow = np.array([0.0004], dtype=np.float64)
+    qgrau = np.array([0.0005], dtype=np.float64)
 
-        t_start = 0
-        timestep = python_config["timesteps"]["COUPLTSTEP"]  # [s]
-        press = np.array([], dtype=np.float64)
-        temp = np.array([], dtype=np.float64)
-        qvap = np.array([], dtype=np.float64)
-        qcond = np.array([], dtype=np.float64)
-        config = pycleo.Config(str(config_filename))
-        microphys = MicrophysicsScheme(
-            config, t_start, timestep, press, temp, qvap, qcond
-        )
+    thermo1 = Thermodynamics(
+        temp1, rho, press1, qvap1, qcond1, qice, qrain, qsnow, qgrau
+    )
+    thermo2 = Thermodynamics(
+        temp2, rho, press2, qvap2, qcond2, qice, qrain, qsnow, qgrau
+    )
 
-        assert microphys.name == "CLEO SDM microphysics"
+    config = pycleo.Config(str(config_filename))
+    microphys = MicrophysicsScheme(
+        config, t_start, timestep, press1, temp1, qvap1, qcond1
+    )
 
-    def test_initialize_wrapper(config_filename):
-        from ruamel.yaml import YAML
+    microphys_wrapped = MicrophysicsSchemeWrapper(
+        config_filename,
+        t_start,
+        timestep,
+        press2,
+        temp2,
+        qvap2,
+        qcond2,
+    )
 
-        yaml = YAML()
-        with open(config_filename, "r") as file:
-            python_config = yaml.load(file)
+    microphys.run(timestep)  # implict change of thermo1
+    thermo2 = microphys_wrapped.run(timestep, thermo2)
 
-        t_start = 0
-        timestep = python_config["timesteps"]["COUPLTSTEP"]  # [s]
-        press = np.array([], dtype=np.float64)
-        temp = np.array([], dtype=np.float64)
-        qvap = np.array([], dtype=np.float64)
-        qcond = np.array([], dtype=np.float64)
-        microphys_wrapped = MicrophysicsSchemeWrapper(
-            config_filename,
-            t_start,
-            timestep,
-            press,
-            temp,
-            qvap,
-            qcond,
-        )
-
-        assert microphys_wrapped.initialize() == 0
-
-    def test_finalize_wrapper(config_filename):
-        from ruamel.yaml import YAML
-
-        yaml = YAML()
-        with open(config_filename, "r") as file:
-            python_config = yaml.load(file)
-
-        t_start = 0
-        timestep = python_config["timesteps"]["COUPLTSTEP"]  # [s]
-        press = np.array([], dtype=np.float64)
-        temp = np.array([], dtype=np.float64)
-        qvap = np.array([], dtype=np.float64)
-        qcond = np.array([], dtype=np.float64)
-        microphys_wrapped = MicrophysicsSchemeWrapper(
-            config_filename,
-            t_start,
-            timestep,
-            press,
-            temp,
-            qvap,
-            qcond,
-        )
-
-        assert microphys_wrapped.finalize() == 0
-
-    def test_microphys_with_wrapper(config_filename):
-        from ruamel.yaml import YAML
-
-        yaml = YAML()
-        with open(config_filename, "r") as file:
-            python_config = yaml.load(file)
-
-        t_start = 0
-        timestep = python_config["timesteps"]["COUPLTSTEP"]  # [s]
-        temp1 = np.array([288.15], dtype=np.float64)
-        temp2 = np.array([288.15], dtype=np.float64)
-        rho = np.array([1.225], dtype=np.float64)
-        press1 = np.array([101325], dtype=np.float64)
-        press2 = np.array([101325], dtype=np.float64)
-        qvap1 = np.array([0.015], dtype=np.float64)
-        qvap2 = np.array([0.015], dtype=np.float64)
-        qcond1 = np.array([0.0001], dtype=np.float64)
-        qcond2 = np.array([0.0001], dtype=np.float64)
-        qice = np.array([0.0002], dtype=np.float64)
-        qrain = np.array([0.0003], dtype=np.float64)
-        qsnow = np.array([0.0004], dtype=np.float64)
-        qgrau = np.array([0.0005], dtype=np.float64)
-
-        thermo1 = Thermodynamics(
-            temp1, rho, press1, qvap1, qcond1, qice, qrain, qsnow, qgrau
-        )
-        thermo2 = Thermodynamics(
-            temp2, rho, press2, qvap2, qcond2, qice, qrain, qsnow, qgrau
-        )
-
-        config = pycleo.Config(str(config_filename))
-        microphys = MicrophysicsScheme(
-            config, t_start, timestep, press1, temp1, qvap1, qcond1
-        )
-
-        microphys_wrapped = MicrophysicsSchemeWrapper(
-            config_filename,
-            t_start,
-            timestep,
-            press2,
-            temp2,
-            qvap2,
-            qcond2,
-        )
-
-        microphys.run(timestep)  # implict change of thermo1
-        thermo2 = microphys_wrapped.run(timestep, thermo2)
-
-        assert thermo1.press == thermo2.press
-        assert thermo1.temp == thermo2.temp
-        for q1, q2 in zip(thermo1.massmix_ratios, thermo2.massmix_ratios):
-            assert q1 == q2
-
-    print(f"MPI version: {MPI.Get_version()}")
-
-    print("TEST 1: test_initialize")
-    test_initialize(config_filename)
-    print("------- TEST 1/4 PASSED -------")
-
-    print("TEST 2: test_initialize_wrapper")
-    test_initialize_wrapper(config_filename)
-    print("------- TEST 2/4 PASSED -------")
-
-    print("TEST 3: test_finalize_wrapper")
-    test_finalize_wrapper(config_filename)
-    print("------- TEST 3/4 PASSED -------")
-
-    print("TEST 4: test_microphys_with_wrapper")
-    test_microphys_with_wrapper(config_filename)
-    print("------- TEST 4/4 PASSED -------")
+    assert thermo1.press == thermo2.press
+    assert thermo1.temp == thermo2.temp
+    for q1, q2 in zip(thermo1.massmix_ratios, thermo2.massmix_ratios):
+        assert q1 == q2
